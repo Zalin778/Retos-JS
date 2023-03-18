77d9253ae7598ce4f1573e43d19ecfff108e2c41
Zalin778 <126732700+Zalin778@users.noreply.github.com>
2023-03-17 19:27:04 -0500

    update

--- promedio/main.js
+++ promedio/main.js
@@ -1,20 +1,59 @@
-function calcularPromedio(){
-    //Personas
-    let proRoberto = 2;
-    let Roberto = ["Nombre: Roberto", proRoberto, "Sexo: M"];
-    let proCarlos = 4;
-    let Carlos = ["Nombre: Carlos", proCarlos, "Sexo: M"];
-    let proKaren = 4.92;
-    let Karen = ["Nombre: Karen", "Promedio: ", proKaren, "Sexo: F",];
+var estudiante = [];
+var promedioMayor = 4;
+var promedioMenor = 1.5;
+var promedioMasculino = 0;
+var promedioFemenino = 0;
+var countMasculino = 0;
+var countFemenino = 0;
+var acumFemenino;
 
-    //Estado
-
-    if (proRoberto || proCarlos || proKaren <= 1.5 & 3){
-        console.log("FP");
-    } else{
-        console.log("nel");
-    }
-
+const evento = (event) => {
+    event.preventDefault();
 }
 
-calcularPromedio();
+function agregarEstudiantes() {
+    var nombre = document.getElementById("nombre").value;
+    var promedio = parseFloat(document.getElementById("promedio").value);
+    var sexo = document.getElementById("sexo").value;
+
+    var estudiantes = {
+        nombre: nombre,
+        promedio: promedio,
+        sexo: sexo
+    };
+
+    estudiante.push(estudiantes);
+
+    document.getElementById("estudiantes").innerText = estudiante;
+
+    if (promedio >= promedioMayor){
+        promedioMayor == promedio;
+        document.getElementById("promedioMayor").innerText = promedioMayor;
+        console.log("promedio mayor", promedioMayor);
+    }
+
+    if (promedio == promedioMenor){
+        promedioMenor == promedio;
+        document.getElementById("promedioMenor").innerText = promedioMenor;
+        console.log("promedio menor", promedioMenor);
+    }
+
+    if (sexo == "M"){
+        promedioMasculino += promedio;
+        countMasculino ++;
+        document.getElementById("promedioMasculino").innerText = promedioMasculino;
+        console.log("promedio masculino", promedioMasculino);
+    }
+
+    else if (sexo == "F"){
+        promedioFemenino += promedio;
+        countFemenino ++;
+        acumFemenino = promedioFemenino / countFemenino;
+        document.getElementById("promedioFemenino").innerHTML = promedioFemenino;
+        console.log("promedio femenino", promedioFemenino);
+    }
+
+    console.log(estudiante);
+
+};
+ agregarEstudiantes
